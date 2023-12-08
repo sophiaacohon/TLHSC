e73de72ca0f41dedaf3e1d21577f5598f40dcb15
unclejambo <jajacampos64@gmail.com>
2023-12-08 23:51:24 +0800

    Admin dashboard data

--- tlhsc-finalproject/src/components/admin/Dashboard.jsx
+++ tlhsc-finalproject/src/components/admin/Dashboard.jsx
@@ -1,118 +1,193 @@
-import { Tab } from '@headlessui/react'
-import React, { useEffect, useState } from 'react'
-import Header from '../partials/Header'
+import { Tab } from '@headlessui/react';
+import React, { useEffect, useState } from 'react';
+import Header from '../partials/Header';
 import axios from 'axios';
 
-import { TbTruckDelivery, TbPackage, TbTruckLoading } from "react-icons/tb";
-import { LuPackageOpen } from "react-icons/lu";
-import { MdOutlineLibraryAddCheck } from "react-icons/md";
-import { MdCancelPresentation } from "react-icons/md";
-import { TbReceiptRefund } from "react-icons/tb";
-
 function classNames(...classes) {
-  return classes.filter(Boolean).join(' ')
+  return classes.filter(Boolean).join(' ');
 }
 
 export default function Example() {
-
   const [dashboardData, setDashboardData] = useState(null);
 
   useEffect(() => {
-    axios.get('http://localhost:3000/dashboard-data')
+    axios
+      .get('http://localhost:3000/dashboard-data')
       .then((response) => {
+        console.log(response.data);
         setDashboardData(response.data);
       })
       .catch((error) => {
-        console.error('fail', error);
+        console.error('Failed to fetch dashboard data:', error);
       });
   }, []);
 
-  let [categories] = useState({
-    ALL: [
-      {},
-    ],
-    BRANCH_1: [
-      {},
-    ],
-    BRANCH_2: [
-      {},
-    ],
-    BRANCH_3: [
-      {},
-    ],
-  })
+  const categories = [
+    {
+      id: 'ALL',
+      title: 'All',
+      counts: [
+        {
+          title: 'Unpacked',
+          count: dashboardData?.deliveries?.unpacked || 0,
+        },
+        {
+          title: 'Packed',
+          count: dashboardData?.deliveries?.packed || 0,
+        },
+        {
+          title: 'Shipped',
+          count: dashboardData?.deliveries?.shipped || 0,
+        },
+        {
+          title: 'Delivered',
+          count: dashboardData?.deliveries?.delivered || 0,
+        },
+        {
+          title: 'Cancelled',
+          count: dashboardData?.deliveries?.cancelled || 0,
+        },
+      ],
+    },
+    {
+      id: 'BRANCH_1',
+      title: 'Branch 1',
+      counts: [
+        {
+          title: 'Unpacked',
+          count: dashboardData?.branchCounts?.BRANCH_1?.unpacked || 0,
+        },
+        {
+          title: 'Packed',
+          count: dashboardData?.branchCounts?.BRANCH_1?.packed || 0,
+        },
+        {
+          title: 'Shipped',
+          count: dashboardData?.branchCounts?.BRANCH_1?.shipped || 0,
+        },
+        {
+          title: 'Delivered',
+          count: dashboardData?.branchCounts?.BRANCH_1?.delivered || 0,
+        },
+        {
+          title: 'Cancelled',
+          count: dashboardData?.branchCounts?.BRANCH_1?.cancelled || 0,
+        },
+      ],
+    },
+    {
+      id: 'BRANCH_2',
+      title: 'Branch 2',
+      counts: [
+        {
+          title: 'Unpacked',
+          count: dashboardData?.branchCounts?.BRANCH_2?.unpacked || 0,
+        },
+        {
+          title: 'Packed',
+          count: dashboardData?.branchCounts?.BRANCH_2?.packed || 0,
+        },
+        {
+          title: 'Shipped',
+          count: dashboardData?.branchCounts?.BRANCH_2?.shipped || 0,
+        },
+        {
+          title: 'Delivered',
+          count: dashboardData?.branchCounts?.BRANCH_2?.delivered || 0,
+        },
+        {
+          title: 'Cancelled',
+          count: dashboardData?.branchCounts?.BRANCH_2?.cancelled || 0,
+        },
+      ],
+    },
+    {
+      id: 'BRANCH_3',
+      title: 'Branch 3',
+      counts: [
+        {
+          title: 'Unpacked',
+          count: dashboardData?.branchCounts?.BRANCH_3?.unpacked || 0,
+        },
+        {
+          title: 'Packed',
+          count: dashboardData?.branchCounts?.BRANCH_3?.packed || 0,
+        },
+        {
+          title: 'Shipped',
+          count: dashboardData?.branchCounts?.BRANCH_3?.shipped || 0,
+        },
+        {
+          title: 'Delivered',
+          count: dashboardData?.branchCounts?.BRANCH_3?.delivered || 0,
+        },
+        {
+          title: 'Cancelled',
+          count: dashboardData?.branchCounts?.BRANCH_3?.cancelled || 0,
+        },
+      ],
+    },
+  ];
 
   return (
     <>
-    <Header/>
-    <main>
-    <div className="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
-    <div className="w-full ">
-      <Tab.Group>
-        <Tab.List className="flex space-x-1 rounded-xl bg-white p-1">
-          {Object.keys(categories).map((category) => (
-            <Tab
-              key={category}
-              className={({ selected }) =>
-                classNames(
-                  'w-full rounded-lg text-md font-medium bg-rose-950 leading-5',
-                  'w-full py-2.5 ring-offset-2 ring-offset-rose-950',
-                  selected
-                    ? 'text-white':
-                    'bg-white text-rose-950 shadow hover:bg-zinc-200 hover:text-rose-950'
-                )
-              }
-            >
-              {category}
-            </Tab>
-          ))}
-        </Tab.List>
-
-
-        <Tab.Panels className="mt-2">
-          {Object.values(categories).map((posts, idx) => (
-            <Tab.Panel
-              key={idx}
-              className={classNames(
-                'rounded-xl bg-white p-3',
-                'ring-white/60 ring-offset-2 ring-offset-blue-400 focus:outline-none focus:ring-2'
-              )}
-            >
-              <ul>
-                {posts.map((post) => (
-                  <li
-                    key={post.id}
-                    className="relative rounded-md p-3 hover:bg-gray-100"
+      <Header />
+      <main>
+        <div className="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
+          <div className="w-full ">
+            <Tab.Group>
+              <Tab.List className="flex space-x-1 rounded-xl bg-white p-1">
+                {categories.map((category) => (
+                  <Tab
+                    key={category.id}
+                    className={({ selected }) =>
+                      classNames(
+                        'w-full rounded-lg text-md font-medium bg-rose-950 leading-5',
+                        'w-full py-2.5 ring-offset-2 ring-offset-rose-950',
+                        selected
+                          ? 'text-white'
+                          : 'bg-white text-rose-950 shadow hover:bg-zinc-200 hover:text-rose-950'
+                      )
+                    }
                   >
-                    <h3 className="text-sm font-medium leading-5">
-                      {post.title}
-                    </h3>
-
-                    <ul className="mt-1 flex space-x-1 text-xs font-normal leading-4 text-gray-500">
-                      <li>{post.date}</li>
-                      <li>&middot;</li>
-                      <li>{post.commentCount} comments</li>
-                      <li>&middot;</li>
-                      <li>{post.shareCount} shares</li>
-                    </ul>
-
-                    <a
-                      href="#"
-                      className={classNames(
-                        'absolute inset-0 rounded-md',
-                        'ring-blue-400 focus:z-10 focus:outline-none focus:ring-2'
-                      )}
-                    />
-                  </li>
+                    {category.title}
+                  </Tab>
                 ))}
-              </ul>
-            </Tab.Panel>
-          ))}
-        </Tab.Panels>
-      </Tab.Group>
-    </div>
-    </div>
-    </main>
+              </Tab.List>
+
+              <Tab.Panels className="mt-2">
+                {categories.map((category) => (
+                  <Tab.Panel
+                    key={category.id}
+                    className={classNames(
+                      'rounded-xl bg-white p-3',
+                      'ring-white/60 ring-offset-2 ring-offset-blue-400 focus:outline-none focus:ring-2'
+                    )}
+                  >
+                    <ul>
+                      {category.counts &&
+                        category.counts.map((countItem, idx) => (
+                          <li
+                            key={idx}
+                            className="relative rounded-md p-3 hover:bg-gray-100"
+                          >
+                            <h3 className="text-sm font-medium leading-5">
+                              {countItem.title}
+                            </h3>
+
+                            <p className="mt-1 text-xs font-normal leading-4 text-gray-500">
+                              Count: {countItem.count}
+                            </p>
+                          </li>
+                        ))}
+                    </ul>
+                  </Tab.Panel>
+                ))}
+              </Tab.Panels>
+            </Tab.Group>
+          </div>
+        </div>
+      </main>
     </>
-  )
+  );
 }
--- tlhsc-finalproject/src/server/app.js
+++ tlhsc-finalproject/src/server/app.js
@@ -1,3 +1,4 @@
+
 const express = require('express');
 const mysql = require('mysql2');
 const cors = require('cors');
@@ -5,8 +6,7 @@ const cors = require('cors');
 const port = 3000;
 const app = express();
 
-app.use(express.json())
-// app.use(cors())
+app.use(express.json());
 app.use(cors());
 
 const db = mysql.createConnection({
@@ -16,11 +16,6 @@ const db = mysql.createConnection({
   database: 'tlhsc_db'
 });
 
-// app.get('/dashboard-data', (req, res) => {
-//   res.send('asd')
-// })
-
-
 db.connect((err) => {
   if (err) {
     console.log('Error connecting to MySQL:', err);
@@ -29,34 +24,67 @@ db.connect((err) => {
   }
 });
 
+app.get('/dashboard-data', async (req, res) => {
+  try {
 
-app.get('/dashboard-data', (req, res) => {
-  const deliveryStatusQuery = 'SELECT COUNT(*) as count FROM delivery';
-
-  db.query(deliveryStatusQuery, (err, deliveryStatusResults) => {
-    if (err) {
-      console.error('Error fetching delivery status:', err);
-      res.status(500).json({ error: 'Internal Server Error' });
-      return;
-    }
+    const deliveryStatusQuery = `
+      SELECT
+        (SELECT COUNT(*) as count FROM delivery WHERE delivery_status = 'UNPACKED') as unpacked,
+        (SELECT COUNT(*) as count FROM delivery WHERE delivery_status = 'PACKED') as packed,
+        (SELECT COUNT(*) as count FROM delivery WHERE delivery_status = 'SHIPPED') as shipped,
+        (SELECT COUNT(*) as count FROM delivery WHERE delivery_status = 'DELIVERED') as delivered,
+        (SELECT COUNT(*) as count FROM delivery WHERE delivery_status = 'CANCELLED') as cancelled
+    `;
 
     const soldProductsQuery = 'SELECT COUNT(*) as count FROM product WHERE product_status = "sold"';
 
-    db.query(soldProductsQuery, (err, soldProductsResults) => {
-      if (err) {
-        console.error('Error fetching sold products:', err);
-        res.status(500).json({ error: 'Internal Server Error' });
-        return;
-      }
+    const [deliveryStatusResults, soldProductsResults] = await Promise.all([
+      db.promise().query(deliveryStatusQuery),
+      db.promise().query(soldProductsQuery),
+    ]);
 
-      const dashboardData = {
-        deliveries: deliveryStatusResults,
-        soldProducts: soldProductsResults[0].count
+    const dashboardData = {
+      deliveries: {
+        unpacked: deliveryStatusResults[0][0].unpacked,
+        packed: deliveryStatusResults[0][0].packed,
+        shipped: deliveryStatusResults[0][0].shipped,
+        delivered: deliveryStatusResults[0][0].delivered,
+        cancelled: deliveryStatusResults[0][0].cancelled,
+      },
+      soldProducts: soldProductsResults[0][0].count,
+    };
+
+    const branchCountsQuery = `
+      SELECT
+        branch_id,
+        (SELECT COUNT(*) as count FROM delivery WHERE branch_id = b.branch_id AND delivery_status = 'UNPACKED') as unpacked,
+        (SELECT COUNT(*) as count FROM delivery WHERE branch_id = b.branch_id AND delivery_status = 'PACKED') as packed,
+        (SELECT COUNT(*) as count FROM delivery WHERE branch_id = b.branch_id AND delivery_status = 'SHIPPED') as shipped,
+        (SELECT COUNT(*) as count FROM delivery WHERE branch_id = b.branch_id AND delivery_status = 'DELIVERED') as delivered,
+        (SELECT COUNT(*) as count FROM delivery WHERE branch_id = b.branch_id AND delivery_status = 'CANCELLED') as cancelled
+      FROM branch b;
+    `;
+
+    const branchCountsResults = await db.promise().query(branchCountsQuery);
+    const branchCounts = {};
+
+    branchCountsResults[0].forEach((branch) => {
+      branchCounts[`BRANCH_${branch.branch_id}`] = {
+        unpacked: branch.unpacked,
+        packed: branch.packed,
+        shipped: branch.shipped,
+        delivered: branch.delivered,
+        cancelled: branch.cancelled,
       };
-
-      res.json(dashboardData);
     });
-  });
+
+    dashboardData.branchCounts = branchCounts;
+
+    res.json(dashboardData);
+  } catch (error) {
+    console.error('Error fetching data:', error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
 });
 
 app.listen(port, () => {
